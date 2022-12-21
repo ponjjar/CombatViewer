0f2ffd8720f3401eefe3d4d494ec47996698da3b
Caique Ponjjar <caiqueponjjar@gmail.com>
2022-12-21 18:02:57 -0300

    Separeted in classes

---
+++ classes/settingsLoader.py
@@ -0,0 +1,9 @@
+
+#Carrega estado das configurações passadas no settings.txt
+arquivo = open('settings.txt', 'r')
+try:
+    linhas = arquivo.readlines()
+    secondMonitor = int(linhas[0].replace("\n", "").split(":")[1])
+finally:
+                #Fechar o arquivo
+                arquivo.close ()
